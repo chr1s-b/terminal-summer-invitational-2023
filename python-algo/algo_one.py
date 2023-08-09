4192d97c532271741d773ddf83ca94bfad8e5bb2
chr1s-b <16579127+chr1s-b@users.noreply.github.com>
2023-08-09 22:19:57 +0100

    new algo

---
+++ python-algo/algo_one.py
@@ -0,0 +1,113 @@
+import gamelib
+import random
+import math # noqa F401
+import warnings # noqa F401
+from sys import maxsize
+import json
+import strategies
+
+
+"""
+Most of the algo code you write will be in this file unless you create new
+modules yourself. Start by modifying the 'on_turn' function.
+
+Advanced strategy tips: 
+
+  - You can analyze action frames by modifying on_action_frame function
+
+  - The GameState.map object can be manually manipulated to create hypothetical 
+  board states. Though, we recommended making a copy of the map to preserve 
+  the actual current map state.
+"""
+
+
+class AlgoStrategy(gamelib.AlgoCore):
+    def __init__(self):
+        super().__init__()
+        seed = random.randrange(maxsize)
+        random.seed(seed)
+        gamelib.debug_write('Random seed: {}'.format(seed))
+
+    def on_game_start(self, config):
+        """ 
+        Read in config and perform any initial setup here 
+        """
+        gamelib.debug_write('Configuring your custom algo strategy...')
+        self.config = config
+        global WALL, SUPPORT, TURRET, SCOUT, DEMOLISHER, INTERCEPTOR, MP, SP
+        WALL = config["unitInformation"][0]["shorthand"]
+        SUPPORT = config["unitInformation"][1]["shorthand"]
+        TURRET = config["unitInformation"][2]["shorthand"]
+        SCOUT = config["unitInformation"][3]["shorthand"]
+        DEMOLISHER = config["unitInformation"][4]["shorthand"]
+        INTERCEPTOR = config["unitInformation"][5]["shorthand"]
+        MP = 1
+        SP = 0
+        # This is a good place to do initial setup
+        self.defenses = strategies.Defenses(config)
+        self.attacks = strategies.Attacks(config)
+        self.openings = strategies.Openings(config)
+        self.utilities = strategies.Utilities(config)
+        self.scored_on_locations = []
+
+    def on_turn(self, turn_state):
+        """
+        This function is called every turn with the game state wrapper as
+        an argument. The wrapper stores the state of the arena and has methods
+        for querying its state, allocating your current resources as planned
+        unit deployments, and transmitting your intended deployments to the
+        game engine.
+        """
+        game_state = gamelib.GameState(self.config, turn_state)
+        gamelib.debug_write('Performing turn {} of your custom algo strategy'.format(game_state.turn_number))
+        game_state.suppress_warnings(True)  #Comment or remove this line to enable warnings.
+
+        self.starter_strategy(game_state)
+
+        game_state.submit_turn()
+
+
+    """
+    NOTE: All the methods after this point are part of the sample starter-algo
+    strategy and can safely be replaced for your custom algo.
+    """
+
+    def starter_strategy(self, game_state):
+        """
+        For defense we will use a spread out layout and some interceptors early on.
+        We will place turrets near locations the opponent managed to score on.
+        For offense we will use long range demolishers if they place stationary units near the enemy's front.
+        If there are no stationary units to attack in the front, we will send Scouts to try and score quickly.
+        """
+        # First, place basic defenses
+        self.defenses.build_defences(game_state)
+        # Now build reactive defenses based on where the enemy scored
+        self.defenses.build_reactive_defense(game_state)
+
+        # If the turn is less than 5, stall with interceptors and wait to see enemy's base
+        if game_state.turn_number < 5:
+            self.defenses.stall_with_interceptors(game_state)
+        else:
+            # Now let's analyze the enemy base to see where their defenses are concentrated.
+            # If they have many units in the front we can build a line for our demolishers to attack them at long range.
+            if self.detect_enemy_unit(game_state, unit_type=None, valid_x=None, valid_y=[14, 15]) > 10:
+                self.attacks.demolisher_line_strategy(game_state)
+            else:
+                # They don't have many units in the front so lets figure out their least defended area and send Scouts there.
+
+                # Only spawn Scouts every other turn
+                # Sending more at once is better since attacks can only hit a single scout at a time
+                if game_state.turn_number % 2 == 1:
+                    # To simplify we will just check sending them from back left and right
+                    scout_spawn_location_options = [[13, 0], [14, 0]]
+                    best_location = self.utilities.least_damage_spawn_location(game_state, scout_spawn_location_options)
+                    game_state.attempt_spawn(SCOUT, best_location, 1000)
+
+                # Lastly, if we have spare SP, let's build some supports
+                support_locations = [[13, 2], [14, 2], [13, 3], [14, 3]]
+                game_state.attempt_spawn(SUPPORT, support_locations)
+
+
+if __name__ == "__main__":
+    algo = AlgoStrategy()
+    algo.start()
