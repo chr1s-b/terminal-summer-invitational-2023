806356b53f565849dcdf5d8fecd79d6066f19824
chr1s-b <16579127+chr1s-b@users.noreply.github.com>
2023-08-09 22:26:17 +0100

    correct indent

--- python-algo/strategies/attacks.py
+++ python-algo/strategies/attacks.py
@@ -16,23 +16,23 @@ class Attacks:
         return
 
     def demolisher_line_strategy(self, game_state):
-            """
-            Build a line of the cheapest stationary unit so our demolisher can attack from long range.
-            """
-            # First let's figure out the cheapest unit
-            # We could just check the game rules, but this demonstrates how to use the GameUnit class
-            stationary_units = [WALL, TURRET, SUPPORT]
-            cheapest_unit = WALL
-            for unit in stationary_units:
-                unit_class = gamelib.GameUnit(unit, game_state.config)
-                if unit_class.cost[game_state.MP] < gamelib.GameUnit(cheapest_unit, game_state.config).cost[game_state.MP]:
-                    cheapest_unit = unit
+        """
+        Build a line of the cheapest stationary unit so our demolisher can attack from long range.
+        """
+        # First let's figure out the cheapest unit
+        # We could just check the game rules, but this demonstrates how to use the GameUnit class
+        stationary_units = [WALL, TURRET, SUPPORT]
+        cheapest_unit = WALL
+        for unit in stationary_units:
+            unit_class = gamelib.GameUnit(unit, game_state.config)
+            if unit_class.cost[game_state.MP] < gamelib.GameUnit(cheapest_unit, game_state.config).cost[game_state.MP]:
+                cheapest_unit = unit
 
-            # Now let's build out a line of stationary units. This will prevent our demolisher from running into the enemy base.
-            # Instead they will stay at the perfect distance to attack the front two rows of the enemy base.
-            for x in range(27, 5, -1):
-                game_state.attempt_spawn(cheapest_unit, [x, 11])
+        # Now let's build out a line of stationary units. This will prevent our demolisher from running into the enemy base.
+        # Instead they will stay at the perfect distance to attack the front two rows of the enemy base.
+        for x in range(27, 5, -1):
+            game_state.attempt_spawn(cheapest_unit, [x, 11])
 
-            # Now spawn demolishers next to the line
-            # By asking attempt_spawn to spawn 1000 units, it will essentially spawn as many as we have resources for
-            game_state.attempt_spawn(DEMOLISHER, [24, 10], 1000)
+        # Now spawn demolishers next to the line
+        # By asking attempt_spawn to spawn 1000 units, it will essentially spawn as many as we have resources for
+        game_state.attempt_spawn(DEMOLISHER, [24, 10], 1000)
