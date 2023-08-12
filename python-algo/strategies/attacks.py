d961c2d381ec30060541597835f842dc751e7328
DarrenLo0530 <darren.lo.0530@gmail.com>
2023-08-12 03:06:49 -0400

    Fix max shielding

--- python-algo/strategies/attacks.py
+++ python-algo/strategies/attacks.py
@@ -98,23 +98,24 @@ class Attacks:
               filtered.append(location)
         return filtered
     
-    def calculate_maximum_shielding(self, game_state):
+    def calculate_shielding_map(self, game_state, player_index):
         grid = [[0 for _ in range(0, game_state.game_map.ARENA_SIZE)] for _ in range(0, game_state.game_map.ARENA_SIZE)]
         for location in game_state.game_map:
-            if len(game_state.game_map[location]) != 0 and game_state.game_map[location][0].unit_type == SUPPORT:
+            if len(game_state.game_map[location]) != 0 and game_state.game_map[location][0].unit_type == SUPPORT and game_state.game_map[location][0].player_index == player_index:
                 support_unit = game_state.game_map[location][0]
                 # Get y position relative to enemy
-                shield_strength = support_unit.shieldPerUnit + support_unit.shieldBonusPerY * (27 - support_unit.y)
+                shield_bonus =  support_unit.shieldBonusPerY * (support_unit.y if player_index == 0 else (27 - support_unit.y)) 
+                shield_strength = support_unit.shieldPerUnit + shield_bonus
                 shield_range = support_unit.shieldRange
                 for shield_location in game_state.game_map.get_locations_in_range(location, shield_range):
                     grid[shield_location[0]][shield_location[1]] += shield_strength
 
-        return max(map(max, grid))
+        return grid
 
     def spawn_intercept(self, game_state):
         # TODO: Change this
         middleIsOpen = True
-        max_shielding = self.calculate_maximum_shielding(game_state)
+        enemy_shielding_map = self.calculate_shielding_map(game_state, player_index=1)
 
         #note: doesn't incldue effects of supports yet (just assuming they're boosted by div by 6, could make 9 if know no supports)
         interceptors = [[0, [0,0]], [0, [0,0]]] #[leftside (num to spawn, where to spawn), rightside (num to spawn, where to spawn)]
@@ -130,6 +131,11 @@ class Attacks:
             minDamage_right = enemy_path_right[1]
             best_location_left = enemy_path_left[0]
             minDamage_left = enemy_path_left[1]
+
+            enemy_path_locations = game_state.find_path_to_edge(enemy_path_left[0]) +  game_state.find_path_to_edge(enemy_path_right[0])
+
+            max_shielding = max([enemy_shielding_map[enemy_path_location[0]][enemy_path_location[1]] for enemy_path_location in enemy_path_locations])
+
             total_health = numPosEnemyAttackers * 20 + max_shielding
             if minDamage_left < total_health:
                 numDeploy = math.ceil(total_health - minDamage_left / 6)
