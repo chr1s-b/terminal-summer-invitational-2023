b51bd72947c73688035b879f7914abf5c7bce5ef
chr1s-b <16579127+chr1s-b@users.noreply.github.com>
2023-08-12 14:02:34 +0100

    always maintain starter defenses

--- python-algo/algo_one.py
+++ python-algo/algo_one.py
@@ -82,11 +82,10 @@ class AlgoStrategyOne(gamelib.AlgoCore):
         destroyed_structures = self.utilities.destroyed_structures
 
         # only try to build the opening if the opening has not been completed yet
-        if not self.five_turret_complete:
-            phase_complete = self.defenses.five_turret(game_state)
-            if phase_complete == 5:
-                self.five_turret_complete = True
-            # TODO send mobile units
+        phase_complete = self.defenses.five_turret(game_state)
+        gamelib.debug_write(f'Opening phase complete: {phase_complete}')
+        if phase_complete == 5:
+            self.five_turret_complete = True
 
         if self.five_turret_complete:
             midgame_phase = self.defenses.build_midgame_defenses(game_state)
