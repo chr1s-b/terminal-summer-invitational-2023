fc770e3b3fb8980e6f741317275ef96049c9a6c1
chr1s-b <16579127+chr1s-b@users.noreply.github.com>
2023-08-12 11:39:32 +0100

    improve opening defense implementation

--- python-algo/strategies/defenses.py
+++ python-algo/strategies/defenses.py
@@ -19,6 +19,53 @@ class Defenses:
         self.phase = 0
         return
 
+    def five_turret(self, game_state):
+        """Builds opening state with four turrets and five walls."""
+        opening_phase = 0
+
+        # turn 1
+        turret_positions = [[3, 12], [6, 12], [25, 12], [18, 9]]
+        wall_positions = [[3, 13], [6, 13], [25, 13], [17, 9], [18, 10]]
+        upgrade_turret_positions = [[3, 12], [6, 12], [25, 12]]
+        phase_1 = [(TURRET, t_pos) for t_pos in turret_positions]
+        + [(WALL, w_pos) for w_pos in wall_positions]
+        + [(UPGRADE, u_pos) for u_pos in upgrade_turret_positions]
+        if not self.build_phase(game_state, phase_1):
+            return
+        opening_phase = 1
+
+        if (game_state.turn_number == 0):
+            return
+
+        # expected turn 2
+        phase_2 = [(UPGRADE, [18, 9])]
+        if not self.build_phase(phase_2):
+            return
+        opening_phase = 2
+
+        # expected turn 2/3
+        # ? do the walls get spawned in list-order priority?
+        wall_positions = [[7, 12], [7, 11], [7, 10], [8, 9], [26, 13], [27, 13], [0, 13], [1, 13], [2, 13], [4, 13], [5, 13]]
+        phase_3 = [(WALL, w_pos) for w_pos in wall_positions]
+        if not self.build_phase(game_state, phase_3):
+            return
+        opening_phase = 3
+
+        # expected turn 4
+        phase_4 = [(TURRET, [21, 11]), (WALL, [21, 12]), (WALL, [22, 12])]
+        if not self.build_phase(phase_4):
+            return
+        opening_phase = 4
+
+        # expected turn 5
+        wall_positions = [[20, 11], [19, 10], [22, 14]] + [[x, 8] for x in range(9, 17)]
+        phase_5 = [(UPGRADE, [21, 11])] + [(WALL, w_pos) for w_pos in wall_positions]
+        if not self.build_phase(phase_5):
+            return
+        opening_phase = 5
+
+        return opening_phase
+
     def build_midgame_defenses(self, game_state):
         """Upgrade and maintain defences in the mid-game."""
         # currently quits when it fails to complete a phase
